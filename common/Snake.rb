4270cfa41fe3658563c218eb8662bcea1757eded
georg wiltschek <georg.wiltschek@gmail.com>
2013-03-20 10:41:53 +0100

    Snake.rb

---
+++ common/Snake.rb
@@ -0,0 +1,78 @@
+class Snake
+	def initialize x, y
+		@pos_x = x
+		@pos_y = y
+		@color = 123456
+		@tail = Array.new
+		@tail.push(self)
+	end
+
+	def move direction
+
+		@tail.last.set_y(@tail.first.get_y)
+		@tail.last.set_x(@tail.first.get_x)
+		
+		if @tail.length > 1 then
+			last = @tail.pop
+			@tail.insert(1, last)
+		end
+
+		case direction
+			when :right
+				@tail.first.set_x(@pos_x = @pos_x + 1)
+			when :left
+				@tail.first.set_x(@pos_x = @pos_x - 1)
+			when :down
+				@tail.first.set_y(@pos_y = @pos_y + 1)
+			when :up
+				@tail.first.set_y(@pos_y = @pos_y - 1)
+		end
+	end
+
+	def update delta, direction
+		if @tail.length < 20 then
+
+			case direction
+				when :right
+					t = Snake.new(@tail.last.get_x - 1, @tail.last.get_y)
+
+				when :left
+					t = Snake.new(@tail.last.get_x + 1, @tail.last.get_y)
+
+				when :up
+					t = Snake.new(@tail.last.get_x,     @tail.last.get_y + 1)
+
+				when :down
+					t= Snake.new(@tail.last.get_x, @tail.last.get_y - 1)
+			end
+
+			if t != nil then
+				@tail.push(t)
+			end
+		end
+	end
+
+	def get_tail
+		return @tail
+	end
+
+	def get_x
+		return @pos_x
+	end
+
+	def get_y
+		return @pos_y
+	end
+	
+	def set_x x
+		@pos_x = x
+	end
+
+	def set_y y
+		@pos_y = y
+	end
+
+	def get_color
+		return @color
+	end
+end
