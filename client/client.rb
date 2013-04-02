eacb5591bd86679bc12c194e5df074ca7078855a
Thomas Handschuh <thomas.handschuh@rise-world.com>
2013-04-03 00:09:53 +0200

    basic OpenGL version

--- client/client.rb
+++ client/client.rb
@@ -5,6 +5,10 @@ require 'logger'
 require 'rubygems'
 require 'json'
 require 'socket'
+require 'opengl'
+require 'gl'
+include Gl,Glu,Glut
+
 
 require "#{File.dirname(__FILE__)}/../common/Snake"
 
@@ -15,10 +19,28 @@ class Client
 		@w = 640 / 8
 		@h = 480 / 8
 
-		SDL.init SDL::INIT_VIDEO
-		
-		@screen	 = SDL::set_video_mode @w * @scale, @h * @scale, 24, SDL::SWSURFACE
-		@BGCOLOR = @screen.format.mapRGB 0, 0, 0
+    # SDL.init SDL::INIT_VIDEO
+    #     SDL.setGLAttr(SDL::GL_DOUBLEBUFFER,1)
+    # 
+    # @screen   = SDL::set_video_mode @w * @scale, @h * @scale, 24, SDL::OPENGL
+    #     # @BGCOLOR = @screen.format.mapRGB 0, 0, 0
+    
+    SDL.init(SDL::INIT_VIDEO)
+    SDL.setGLAttr(SDL::GL_DOUBLEBUFFER,1)
+    SDL.setVideoMode(@w * @scale, @h * @scale,32,SDL::OPENGL | SDL::GL_DOUBLEBUFFER | SDL::HWSURFACE)
+    glViewport(0,0,@w * @scale, @h * @scale)
+    
+    GL.ClearColor(0.0, 1.0, 0.0, 0.0)
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    glOrtho(0, @w * @scale, @h * @scale, 0, 1, -1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity()
+    
+    # GL.ClearDepth(1.0)
+    
 
 		@log = Logger.new(STDOUT)
 
@@ -63,18 +85,51 @@ class Client
 
 	# draws all the snakes
 	def draw snakes
+    draw_opengl snakes
 		# TODO maybe check the SDL documentation, since the game tends
 		# to stop redrawing after a while
-		@screen.fill_rect 0, 0, @w * @scale, @h * @scale, @BGCOLOR
-
-		snakes.each do |snake|
-			snake.get_tail.each do |t|
-				@screen.fill_rect t.x * @scale, t.y * @scale, 8, 8,t.color
-			end
-		end
-
-		@screen.flip	
+    # @screen.fill_rect 0, 0, @w * @scale, @h * @scale, @BGCOLOR
+    # 
+    # snakes.each do |snake|
+    #   snake.get_tail.each do |t|
+    #     @screen.fill_rect t.x * @scale, t.y * @scale, 8, 8,t.color
+    #   end
+    # end
+    # 
+    # @screen.flip  
 	end
+  
+  def gl_fill_rect x,y,w,h,color
+    glColor 1,0,0
+    glBegin Gl::GL_POLYGON
+    # glColor3f( 1.0, 0.0, 0.0 )
+  	glVertex2f( x, y )
+    # glColor3f( 0.0, 1.0, 0.0 )
+  	glVertex2f( x,  y+h )
+    # glColor3f( 0.0, 0.0, 1.0 )
+  	glVertex2f(  x+h,  y+h )
+    # glColor3f( 1.0, 0.0, 1.0 )
+  	glVertex2f(  x+h, y )
+  	glEnd
+    
+  end
+  
+  def draw_opengl snakes
+    puts "draw"
+    GL.Clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT)
+    # GL.MatrixMode(GL::PROJECTION)
+    GL.LoadIdentity()
+    # perspective(projectionmatrix, 45.0, 1.0, 0.1, 100.0)
+    # gl_fill_rect 0,0,100,100, 0
+    
+    snakes.each do |snake|
+      snake.get_tail.each do |t|
+        gl_fill_rect t.x * @scale, t.y * @scale, 8, 8,t.color
+      end
+    end
+    
+    SDL.GL_swap_buffers
+  end
 
   def connect_to_server
     @socket = TCPSocket.open("localhost", 9876)
